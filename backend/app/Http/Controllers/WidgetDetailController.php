6739798695851fcd1487a3f9edb2968ac373a192
looloops <sota.luljeta@gmail.com>
2024-06-04 13:33:13 +0200

    first commit

---
+++ backend/app/Http/Controllers/WidgetDetailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WidgetDetail;
+use App\Http\Requests\StoreWidgetDetailRequest;
+use App\Http\Requests\UpdateWidgetDetailRequest;
+
+class WidgetDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreWidgetDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WidgetDetail $widgetDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WidgetDetail $widgetDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateWidgetDetailRequest $request, WidgetDetail $widgetDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WidgetDetail $widgetDetail)
+    {
+        //
+    }
+}
